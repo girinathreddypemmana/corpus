5ebe42cae5c3485ee8e2686c85aee89acd2b9be3
pemmanagirinathreddy <43847051+pemmanagirinathreddy@users.noreply.github.com>
2018-10-08 13:17:18 +0530

    base code
    base code

---
+++ WebSocketHtmlAppl/ReadMe.txt
@@ -0,0 +1,17 @@
+Softwares required:
+1.GlishFish4.1.1 Server
+2.NetBeans IDE
+
+Steps to run web socket application
+1.check out the code and save in local folder (ex: D:/WebSocket/)
+2.open the NetBeansIDE 
+3.open new project->select java web -> select web application with existing source -> click on next
+4.Location : select the folder where you checked out the code (Ex:D:/WebSocket/WebSocketHtmlAppl)
+  Project Name : WebSocketHtmlAppl
+  Project folder : D:/WebSocketHtml2 (ex: select any local folder) 
+  click on Next
+5 select GlassFish server 4.1.1 click on next
+6.WEB-INF content : browse web-inf content folder of D:/WebSocket/WebSocketHtmlAppl/web/WEB-INF
+	click on finish
+7. select the application-> right click ->click on run 
+8.The application opens in browser 
---
+++ WebSocketHtmlAppl/src/java/org/websocket/websocket/WebSocketServer.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.websocket.websocket;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import org.websocket.model.Item;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author girinathr
+ */
+@ApplicationScoped
+@ServerEndpoint("/actions")
+public class WebSocketServer {
+    @Inject
+    private SessionHandler sessionHandler;
+    
+    @OnOpen
+    public void open(Session session) {
+        sessionHandler.addSession(session);
+    }
+
+     @OnClose
+    public void close(Session session) {
+        sessionHandler.removeSession(session);
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        Logger.getLogger(WebSocketServer.class.getName()).log(Level.SEVERE, null, error);
+    }
+
+    @OnMessage
+    public void handleMessage(String message, Session session) {
+
+        try (JsonReader reader = Json.createReader(new StringReader(message))) {
+            JsonObject jsonMessage = reader.readObject();
+
+            if ("add".equals(jsonMessage.getString("action"))) {
+                Item device = new Item();
+                device.setName(jsonMessage.getString("name"));
+                device.setDescription(jsonMessage.getString("description"));
+                device.setType(jsonMessage.getString("type"));
+                device.setStatus("Off");
+                sessionHandler.addDevice(device);
+            }
+
+            if ("remove".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.removeDevice(id);
+            }
+
+            if ("toggle".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.toggleDevice(id);
+            }
+        }
+    }
+}
